bb728349987a7198d88bcbef527081bdc38d2bec
Elmenabawy <ahhossam68@gmail.com>
2025-08-27 01:35:51 +0300

    Initial commit with project files

--- data/index.ts
+++ data/index.ts
@@ -72,35 +72,46 @@ export const gridItems = [
 export const projects = [
     {
         id: 1,
-        title: "3D Solar System Planets to Explore",
-        des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-        img: "/p1.svg",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-        link: "/ui.earth.com",
+    title: "GoGreen – Solar Power Monitoring Dashboard",
+    des: "Developed a responsive web dashboard to monitor solar power system consumption, analyze usage patterns, and predict future energy needs using real-time data.",
+    img: "/gogreen.svg",
+    iconLists: [
+      "/re.svg",
+      "/bootstrap-5-logo-icon.svg",
+      "/axios.svg",
+      "/react-router-svgrepo-com.svg",
+    ],
+    link: "https://go-green-1.netlify.app/",
     },
     {
-        id: 2,
-        title: "Yoom - Video Conferencing App",
-        des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-        img: "/p2.svg",
-        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-        link: "/ui.yoom.com",
-    },
+  id: 2,
+  title: "FreshCart – E-commerce Shopping Platform",
+  des: "A responsive and modern e-commerce website built with React, featuring product browsing, cart management, and a seamless shopping experience. Designed for performance and usability.",
+  img: "/FreshCart.png",
+  iconLists: [
+    "/re.svg",
+    "/bootstrap-5-logo-icon.svg",
+    "/axios.svg",
+    "/react-router-svgrepo-com.svg",
+  ],
+  link: "https://shopify-fresh-cart.netlify.app/",
+},
     {
-        id: 3,
-        title: "AI Image SaaS - Canva Application",
-        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/p3.svg",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-        link: "/ui.aiimg.com",
-    },
+  id: 3,
+  title: "Custom Salla Store Development",
+  des: "Customized and enhanced multiple e-commerce stores on the Salla platform. Worked on themes, layouts, and integrations to deliver tailored shopping experiences for diverse clients.",
+  img: "/Salla.png",
+  iconLists: ["/Javascript.svg", "/CSS3.svg"],
+
+},
+
     {
         id: 4,
-        title: "Animated Apple Iphone 3D Website",
-        des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-        img: "/p4.svg",
-        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-        link: "/ui.apple.com",
+        title: "Creative Business Website on WordPress",
+des: "Developed a visually appealing WordPress website for a modern brand, incorporating smooth animations and a user-friendly layout.",
+        img: "/wordpress.svg",
+        iconLists: ["/Javascript.svg", "/CSS3.svg", "/woocommerce.svg", "/Astra.svg", "/elementor.png"],
+        
     },
 ];
 
@@ -202,16 +213,14 @@ export const workExperience = [
 ];
 
 export const socialMedia = [
-    {
-        id: 1,
-        img: "/git.svg",
-    },
-    {
-        id: 2,
-        img: "/twit.svg",
-    },
-    {
-        id: 3,
-        img: "/link.svg",
-    },
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Elmenabawy",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/menabawy/",
+  },
 ];
